7872871d36f861f45df0b1d35b8bf276e259ac3b
Francisco Adasme <francisco.adasme@gmail.com>
2022-05-05 18:45:12 -0400

    Add IndexableView

---
+++ spec/indexable_view_spec.cr
@@ -0,0 +1,35 @@
+require "./spec_helper"
+
+describe IndexableView do
+  it "views into an array" do
+    view = IndexableView(StaticArray(Int32, 4), Int32).new StaticArray[1, 2, 3, 4]
+    view.size.should eq 4
+    view[0].should eq 1
+    view[1].should eq 2
+    view[2].should eq 3
+    view[3].should eq 4
+  end
+
+  describe "#<=>" do
+    it "compares with an indexable" do
+      arr = IndexableView(StaticArray(Int32, 4), Int32).new StaticArray[1, 2, 3, 4]
+      (arr <=> [1, 2, 3, 4]).should eq 0
+      (arr <=> [4, 3, 2, 1]).should eq -1
+      (arr <=> [-1, -2, -3, -4]).should eq 1
+    end
+  end
+
+  describe "#inspect" do
+    it "returns the string representation" do
+      arr = IndexableView(StaticArray(Int32, 4), Int32).new StaticArray[1, 2, 3, 4]
+      arr.inspect.should eq "IndexableView(StaticArray[1, 2, 3, 4])"
+    end
+  end
+
+  describe "#to_s" do
+    it "returns the string representation of the array" do
+      arr = IndexableView(StaticArray(Int32, 4), Int32).new StaticArray[1, 2, 3, 4]
+      arr.to_s.should eq "StaticArray[1, 2, 3, 4]"
+    end
+  end
+end
---
+++ src/views/indexable_view.cr
@@ -0,0 +1,59 @@
+# An `IndexableView` is a read-only version of any indexable type. This
+# is useful to give read access to a mutable indexable type such as an
+# array in a type-safe manner.
+#
+# The functionality of `IndexableView` is limited by design, as it just
+# implements the `Indexable` mixin requirements by delegating to the
+# enclosed object.
+#
+# ```
+# arr = StaticArray[1, 2, 3, 4]
+# view = IndexableView(StaticArray(Int32, 4)).new(arr)
+# view.size            # => 4
+# view[2]              # => 3
+# view.select &.even?  # => [2, 4]
+# view.select! &.even? # does not compile
+# view.sort            # does not compile
+# ```
+struct IndexableView(T, U)
+  include Indexable(U)
+  include Comparable(Indexable(U))
+
+  def initialize(@indexable : T)
+  end
+
+  def ==(rhs : Indexable(U)) : Bool
+    equals?(rhs) { |x, y| x == y }
+  end
+
+  def <=>(rhs : Indexable(U)) : Int32
+    min_size = Math.min(size, rhs.size)
+    0.upto(min_size - 1) do |i|
+      n = unsafe_fetch(i) <=> rhs.to_unsafe[i]
+      return n if n != 0
+    end
+    size <=> rhs.size
+  end
+
+  def <=>(rhs : self) : Int32
+    @indexable <=> rhs
+  end
+
+  def inspect(io : IO) : Nil
+    io << self.class.name.partition('(')[0] << '('
+    to_s io
+    io << ')'
+  end
+
+  def size : Int32
+    @indexable.size
+  end
+
+  def to_s(io : IO) : Nil
+    @indexable.to_s io
+  end
+
+  def unsafe_fetch(index : Int) : U
+    @indexable.unsafe_fetch(index)
+  end
+end
